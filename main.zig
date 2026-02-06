f23f210ed63d711aaaf15493464ee9d3763eb9f1
ghost <10137+ghost@users.noreply.github.com>
2026-02-06 16:05:46 -0700

    matmul and sort. So fast that I don't really need libraries for now, focus on algs

--- main.zig
+++ main.zig
@@ -1,10 +1,17 @@
 const std = @import("std");
 
 const LENGTH = 1024 * 1024 * 5;
+var ns: [LENGTH]u32 = undefined;
 var xs: [LENGTH]f32 = undefined;
 var ys: [LENGTH]f32 = undefined;
 var zs: [LENGTH]f32 = undefined;
-var ns: [LENGTH]u32 = undefined;
+
+fn rand(u: *u32, v: *u32) u32 {
+    v.* = 36969 * (v.* & 65535) + (v.* >> 16);
+    u.* = 18000 * (u.* & 65535) + (u.* >> 16);
+    return (v.* << 16) + (u.* & 65535);
+
+}
 
 pub fn main() void {
     // pre-populate randomish z
@@ -12,51 +19,73 @@ pub fn main() void {
     var u: u32 = @truncate(@as(u64, @bitCast(std.time.milliTimestamp())));
     var v: u32 = u % 65536 ;
     for (0..LENGTH) |i| {
-        v = 36969 * (v & 65535) + (v >> 16);
-        u = 18000 * (u & 65535) + (u >> 16);
-        const z: f32 = @floatFromInt((v << 16) + (u & 65535));
-        xs[i] = z;
-        ys[i] = z;
-        zs[i] = z;
         ns[i] = @truncate(i);
+        xs[i] = @floatFromInt(rand(&u, &v));
+        ys[i] = @floatFromInt(rand(&u, &v));
+        zs[i] = @floatFromInt(rand(&u, &v));
     }
 
     for (0..10) |i| {
-        std.debug.print("{any},{any},{any},{any}\n", .{ xs[i], ys[i], zs[i], ns[i] });
+        std.debug.print("{any},{any},{any},{any}\n", .{ ns[i], xs[i], ys[i], zs[i] });
     }
 
+
+    const M: [9]f32 = .{
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9,
+    };
+
+    const t0 = std.time.milliTimestamp();
+    rotate_inplace(M, &xs, &ys, &zs);
+    const t1 = std.time.milliTimestamp();
+    sort_inplace(&ns, &xs, &ys, &zs);
+    const t2 = std.time.milliTimestamp();
+    const dt1: f32 = @floatFromInt(t1 - t0);
+    const dt2: f32 = @floatFromInt(t2 - t1);
+
+    std.debug.print("matmul in {d} sec:\n", .{dt1 / 1000});
+    std.debug.print("sorted in {d} sec:\n", .{dt2 / 1000});
+
+    for (0..10) |i| {
+        std.debug.print("{any},{any},{any},{any}\n", .{ ns[i], xs[i], ys[i], zs[i] });
+    }
+}
+
+
+fn rotate_inplace(M: [9]f32, X: []f32, Y: []f32, Z: []f32) void {
+    for (X, Y, Z) |*x, *y, *z| {
+        const x0, const y0, const z0 = .{x.*, y.*, z.*};
+        x.* = M[0]*x0 + M[1]*y0 + M[2]*z0;
+        y.* = M[3]*x0 + M[4]*y0 + M[5]*z0;
+        z.* = M[6]*x0 + M[7]*y0 + M[8]*z0;
+    }
+}
+
+fn sort_inplace(N: []u32, X: []f32, Y: []f32, Z: []f32) void {
     const SortQuery = struct {
+        ns: []u32,
         xs: []f32,
         ys: []f32,
         zs: []f32,
-        ns: []u32,
 
         pub fn lessThan(ctx: @This(), a: usize, b: usize) bool {
             return ctx.zs[a] < ctx.zs[b];
         }
 
         pub fn swap(ctx: @This(), a: usize, b: usize) void {
+            std.mem.swap(u32, &ctx.ns[a], &ctx.ns[b]);
             std.mem.swap(f32, &ctx.xs[a], &ctx.xs[b]);
             std.mem.swap(f32, &ctx.ys[a], &ctx.ys[b]);
             std.mem.swap(f32, &ctx.zs[a], &ctx.zs[b]);
-            std.mem.swap(u32, &ctx.ns[a], &ctx.ns[b]);
         }
     };
     const query: SortQuery = .{
-        .xs = &xs,
-        .ys = &ys,
-        .zs = &zs,
-        .ns = &ns,
+        .ns = N,
+        .xs = X,
+        .ys = Y,
+        .zs = Z,
     };
 
-    const before = std.time.milliTimestamp();
     std.sort.heapContext(0, LENGTH, query);
-    const after = std.time.milliTimestamp();
-    const dt: f32 = @floatFromInt(after - before);
-
-    std.debug.print("sorted in {d} sec:\n", .{dt / 1000});
-
-    for (0..10) |i| {
-        std.debug.print("{any},{any},{any},{any}\n", .{ xs[i], ys[i], zs[i], ns[i] });
-    }
 }
