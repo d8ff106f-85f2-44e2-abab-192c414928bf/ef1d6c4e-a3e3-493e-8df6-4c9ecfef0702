eaeda761f4f65b44dcf9986ac25c21ac1abfce49
ghost <10137+ghost@users.noreply.github.com>
2026-02-14 13:12:28 -0700

    minor quat bugfix

--- src/main.zig
+++ src/main.zig
@@ -250,7 +250,7 @@ const Quat = struct {
         const aa = a[0]*a[0] + a[1]*a[1] + a[2]*a[2];
         const bb = b[0]*b[0] + b[1]*b[1] + b[2]*b[2];
         const cc = c[0]*c[0] + c[1]*c[1] + c[2]*c[2];
-        if (cc == 0) return .{};
+        if (aa == 0 or bb == 0) return .{};
         return .{
             .i = c[0] / @sqrt(aa * bb),
             .j = c[1] / @sqrt(aa * bb),
