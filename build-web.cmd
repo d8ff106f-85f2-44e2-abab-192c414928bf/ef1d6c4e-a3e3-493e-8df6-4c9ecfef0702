a144ae4348d22a29f0ff37bfc5961921d199e1ee
ghost <10137+ghost@users.noreply.github.com>
2026-02-17 05:45:07 -0700

    move files

--- build-web.cmd
+++
@@ -1,6 +0,0 @@
-set emsdk_dir=..\emsdk
-%emsdk_dir%\emsdk activate latest && (
-    embuilder build sysroot && embuilder build emdawnwebgpu && (
-        zig build -Dtarget=wasm32-emscripten -Doptimize=ReleaseFast --sysroot %emsdk_dir%\upstream\emscripten\cache\sysroot
-    )
-)
--- wasm-test/build.cmd
+++
@@ -1,2 +0,0 @@
-zig build-exe wasm-parser.zig -target wasm32-freestanding -fno-entry -rdynamic --import-memory -O ReleaseFast
-zig build-exe wasm-slicer.zig -target wasm32-freestanding -fno-entry -rdynamic --import-memory -O ReleaseFast
