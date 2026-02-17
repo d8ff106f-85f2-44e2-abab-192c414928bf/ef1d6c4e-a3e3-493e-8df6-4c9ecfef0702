f75987f776608aa9975f5f5d1f5b3b4de4b977a0
ghost <10137+ghost@users.noreply.github.com>
2026-02-17 05:55:01 -0700

    build

--- build.cmd
+++ build.cmd
@@ -1,4 +1,6 @@
+cd src
 deno run build
 cd dist
 zig build-exe ../wasm-parser.zig -target wasm32-freestanding -fno-entry -rdynamic --import-memory -O ReleaseFast
 zig build-exe ../wasm-slicer.zig -target wasm32-freestanding -fno-entry -rdynamic --import-memory -O ReleaseFast
+cd ../../
